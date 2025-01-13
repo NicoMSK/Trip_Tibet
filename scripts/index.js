b6a2e0fbcc7ffa520f02fdea1bcfc9952f196de2
NicoMSK <158609660+NicoMSK@users.noreply.github.com>
2025-01-13 19:42:10 +0300

    Стилизовал Header (#2)
    * Добавил и подключил фавиконки и проверил в валидпторе, поправил ошибки
    
    * Создал файл для стилей и наполнил его фалайми для каждого блока и поправил html
    
    * Добавил: переменных, мексинов, шрифтов,  поправил формат шрифтов
    
    * Поправил пути для scss файлов
    
    * Написал стили для блока header и адаптировал его под разные экраны

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
 //Переключение меню
 const navMain = document.querySelector('.nav');
 const navToggle = document.querySelector('.nav__toggle');
+const body = document.querySelector('body');
 
 navMain.classList.remove('nav--nojs');
 
@@ -9,31 +10,12 @@ navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('nav--closed')) {
     navMain.classList.remove('nav--closed');
     navMain.classList.add('nav--opened');
+    body.classList.add('page__body-no-scroll');
   } else {
     navMain.classList.add('nav--closed');
     navMain.classList.remove('nav--opened');
+    body.classList.remove('page__body-no-scroll');
   }
 });
 
-const linkFilterToggle = document.querySelectorAll('.catalog__link-filter');
-const linkCurrent = document.querySelector('.catalog__link-filter--current');
-const card = document.querySelector('.card');
-const france = document.querySelector('.card--france');
-const germany = document.querySelector('.card--germany');
-const england = document.querySelector('.card--england');
 
-
-// linkFilterToggle.addEventListener('click', () => {
-//   // if (card.classList.contains('germany')) {
-//   //   card.classList.remove('germany')
-//   // } else {
-//   //   card.classList.add('france')
-//   // }
-//   console.log('hi')
-// })
-for (let i = 0; i < linkFilterToggle.length; i++) {
-  i.onclick = function () {
-    console.log('hi')
-  }
-
-}
